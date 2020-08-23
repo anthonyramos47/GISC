1e0347784492b75dd478cf11ddba016fcee50210
anthonyramos47 <anthonyramos47@gmail.com>
2020-08-22 20:04:17 -0500

    Modification Wrapper_par for data taking

--- Wrapper_par.py
+++ Wrapper_par.py
@@ -4,51 +4,43 @@ import time
 from mpi4py import MPI
 import numpy as np
 
-comm = MPI.COMM_WORLD
-size = comm.Get_size()
-rank = comm.Get_rank()
+comm = MPI.COMM_WORLD # Communicador de MPI 
+size = comm.Get_size() # Numero total de Procesadores
+rank = comm.Get_rank() # Id de cada procesador
 
+# Topologia de la red
 SN = SquareNetwork(height = 40, width  = 40)
 
-step = 12//size
 
-f = open("Data_SN_"+str(rank)+".dat","w")
-t = time.time()     
-print('init:',((rank)*step + 1))
-print('final:',((rank+1)*step +1 ))
-for i in range(((rank)*step + 1), ((rank+1)*step +1 )):
-     F = 10
-     Q = 1 - (1 - 1/i)**F
-     averageS1 = 0
-     averageS2 = 0
+# Tipos de datos por media [(B,B2)]
+# Datos a correr por Anthony
+medias_ar = [(0.25, 0.25), (0.1, 0.5)]
+# Datos a correr por Oscar
+# medias_oscar = [(0.33,0.33), (0.1, 0,8), (0.02, 0.02)]
+
+# Datos Anthony
+f = open("Data_SN_"+str(medias_ar[rank][0])+"_"+str(medias_ar[rank][1])+".dat","w")
+# Datos Oscar
+# f = open("Data_SN_"+str(medias_oscar[rank][0])+"_"+str(medias_oscar[rank][1])+".dat","w")
+
+for i in range( 1 , 1001, 10 ):
+     F = 10 # Longitud de los vectores de opinion
+     Q = 1 - (1 - 1/i)**F # Valor modificado de q solo para plot
+     averageS1 = 0 # Porcion Promedio de la poblacion mas grande
+     averageS2 = 0 # Porcion Promedio de la segunda poblacion mas grande
      for j in range(5):
-          dyn = TwoMedia(graph=SN, tot_par = F, tot_opt = i, B=0.8, B2=0, n_ch=1, T=500)
+          # Inicializamos la dinamica
+          dyn = TwoMedia(graph=SN, tot_par = F, tot_opt = i, B=medias_ar[rank][0], B2=medias_ar[rank][1], n_ch=1, T=500)
+          # Inicializamos la toma de datos
           dic = dyn.get_data()
           averageS1 +=dic[0][1]/800
           averageS2 +=dic[1][1]/800 
+     # Sacamos un promedio de los datos tomados
      averageS1 /= 5
      averageS2 /= 5
+     # Cadena a escribir en el archivo
      cad = str(Q)+" "+str(averageS1)+" "+str(averageS2)+"\n"
+
      f.write(cad)
-t_end = time.time()
-print("Rank:",rank,"Tiempo:",(t_end-t))
 f.close
    
-
-
-#CN = CompleteNetwork(N = 4)
-#CN.draw_network()
-#f = open("sq_net_time.dat","w")
-#for n in range(2,500):
-#     t_g = time.time()
-
-#      t_end = time.time()
-#      f.write(str(n)+" "+str(t_end - t_g)+"\n")
-# f.close()
-#SN.draw_network()
-
-#SFN = ScaleFreeNetwork(100)
-#SFN.draw_network()
-
-#SWN = SmallWorldNetwork(N = 50,K = 5,p = 1)
-#SWN.draw_network(with_labels = True)
