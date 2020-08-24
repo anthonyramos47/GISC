19493bd3d5943e8e8853e6ea58abc4e651db9f29
anthonyramos47 <anthonyramos47@gmail.com>
2020-08-23 21:15:32 -0500

    Changes Wrapper_par

--- Wrapper_par.py
+++ Wrapper_par.py
@@ -11,30 +11,30 @@ rank = comm.Get_rank() # Id de cada procesador
 # Topologia de la red
 SN = SquareNetwork(height = 40, width  = 40)
 
-
 # Tipos de datos por media [(B,B2)]
 # Datos a correr por Anthony
-medias_ar = [(0.25, 0.25), (0.1, 0.5)]
+medias = [(0.25, 0.25), (0.1, 0.5), (0.1, 0.8)]
 # Datos a correr por Oscar
-# medias_oscar = [(0.33,0.33), (0.1, 0,8), (0.02, 0.02)]
+# medias_ar = [(0.1, 0.8)]
 
 # Datos Anthony
-f = open("Data_SN_"+str(medias_ar[rank][0])+"_"+str(medias_ar[rank][1])+".dat","w")
+f = open("Data_SN_"+str(medias[rank][0])+"_"+str(medias[rank][1])+".dat","w")
 # Datos Oscar
 # f = open("Data_SN_"+str(medias_oscar[rank][0])+"_"+str(medias_oscar[rank][1])+".dat","w")
-
-for i in range( 1 , 1001, 10 ):
+step = 0.009
+for i in range( 1 , 100):
      F = 10 # Longitud de los vectores de opinion
-     Q = 1 - (1 - 1/i)**F # Valor modificado de q solo para plot
+     Q = step*i # Valor Q
+     q = int(-(1/(-1 + (1 - Q)**(1/F)))) # Obtenemos q a partir de Q
      averageS1 = 0 # Porcion Promedio de la poblacion mas grande
      averageS2 = 0 # Porcion Promedio de la segunda poblacion mas grande
      for j in range(5):
           # Inicializamos la dinamica
-          dyn = TwoMedia(graph=SN, tot_par = F, tot_opt = i, B=medias_ar[rank][0], B2=medias_ar[rank][1], n_ch=1, T=200)
+          dyn = TwoMedia(graph=SN, tot_par = F, tot_opt = q, B=medias[rank][0], B2=medias[rank][1], n_ch=1, T=200)
           # Inicializamos la toma de datos
           dic = dyn.get_data()
-          averageS1 +=dic[0][1]/800
-          averageS2 +=dic[1][1]/800 
+          averageS1 +=dic[0][1]/1600
+          averageS2 +=dic[1][1]/1600 
      # Sacamos un promedio de los datos tomados
      averageS1 /= 5
      averageS2 /= 5
---
+++ test.py
@@ -0,0 +1,3 @@
+
+for Q in range( 0.0001, 2, 0.009 ):
+    print(Q)
